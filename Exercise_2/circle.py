1d705dcf50ea0f98c796fcfc3debc9c40832c7ef
JL1829 <johnnylou89@icloud.com>
2020-09-04 16:31:30 +0800

    adding new file

---
+++ Exercise_2/circle.py
@@ -0,0 +1,27 @@
+import math
+
+"""
+class Circle(object):
+    #Circle with radius, area and diameter.
+
+    def __init__(self, radius=1):
+        self.radius = radius
+        self.diameter = self.radius * 2
+        self.area = math.pi * self.radius ** 2
+    
+    def __repr__(self):
+        return f"Circle({self.radius})"
+"""
+
+class Circle(object):
+
+    def __init__(self, radius=1):
+        self.radius = radius
+        self.diameter = self.radius * 2
+        self.area = self._area(radius)
+    
+    def _area(self, radius):
+        return math.pi * radius ** 2
+
+    def __repr__(self):
+        return f"Circle({self.radius})"
